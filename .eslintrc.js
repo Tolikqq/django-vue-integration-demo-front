97708e1d1e171f0c65104077bd154a0d87946da0
Kruglov Anatoliy <anatoliy.kruglov@mail.msk>
2020-12-12 16:20:59 +0300

    off

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,16 @@ module.exports = {
     node: true,
   },
   extends: [
-    // 'plugin:vue/essential',
+    'plugin:vue/essential',
     // '@vue/airbnb',
   ],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'indent': 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 'off'
    
   },
 };
